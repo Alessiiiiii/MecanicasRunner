bdf7d761609bd7beaf1953926b6454bb7ecdda5c
Alessiiiiii <alexisgor2000@hotmail.com>
2025-04-21 19:21:17 -0300

    OKK

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -1,36 +1,106 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-
-   
-
-public class PlayerController : MonoBehaviour
+public class rigidbody : MonoBehaviour
 {
-    public float lateralSpeed = 5f; // Velocidad de movimiento lateral
-    public float jumpForce = 5f;   // Fuerza del salto
-    public LayerMask Piso;  // Capa del suelo para detecci�n de colisiones
-    private Rigidbody rb;
-    private bool isGrounded;
+    public float speed;
+    public float jumpForce;
+    public Vector2 inputVector;
+    public Vector3 velocity;
+    public Rigidbody Rigidbody;
+    public float velocityMagnitude;
+    public bool CanJump;
+    public int collectedItems;
 
+    public TMPro.TextMeshProUGUI scoreText;
+    public TMPro.TextMeshProUGUI warningText;
+
+    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        warningText.enabled = false;
+        Rigidbody = GetComponent<Rigidbody>();
+        CanJump = true;
+
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // Movimiento lateral
-        float horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 lateralMovement = new Vector3(horizontalInput * lateralSpeed, rb.linearVelocity.y, 0);
-        rb.linearVelocity = lateralMovement;
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.y = Input.GetAxis("Vertical");
 
-        // Salto
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f, Piso);
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        Rigidbody.AddForce(inputVector.x * speed, 0f, inputVector.y * speed, ForceMode.Impulse);
+
+        velocity = Rigidbody.linearVelocity;
+        velocityMagnitude = velocity.magnitude;
+
+        if (Input.GetKeyDown(KeyCode.Space) && CanJump)
+
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            Rigidbody.AddForce(0f, jumpForce, 0f, ForceMode.Impulse);
+            CanJump = false;
         }
     }
-}
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Choque cotra : " + collision.gameObject.name);
 
+        if (collision.gameObject.CompareTag("Piso"))
+        {
+            CanJump = true;
+        }
+
+        if (collision.gameObject.CompareTag("KillZone"))
+        {
+            Debug.Log("Kill Mee");
+            SceneManager.LoadScene(0);
+        }
+        if (collision.gameObject.CompareTag("Goal"))
+        {
+            SceneManager.LoadScene("Win");
+        }
+
+        if (collision.gameObject.CompareTag("Item"))
+        {
+            Destroy(collision.gameObject);
+
+            collectedItems++;
+            scoreText.text = collectedItems.ToString();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger Enter:" + other.gameObject.name);
+        if (other.gameObject.CompareTag("Item"))
+        {
+            Destroy(other.gameObject);
+            collectedItems++;
+            scoreText.text = collectedItems.ToString();
+        }
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            warningText.enabled = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Trigger Exit:" + other.gameObject.name);
+
+
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            warningText.enabled = false;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Trigger Stay:" + other.gameObject.name);
+    }
+
+}
